64908097385d419289987757758994958696ffa1
neeraj0631 <136948183+neeraj0631@users.noreply.github.com>
2023-08-06 14:09:17 +0530

    module.sol

--- module.sol
+++ module.sol
@@ -2,27 +2,25 @@
 pragma solidity ^0.8.0;
 
 contract ErrorHandlerContract {
-    function requireCheck(uint _stock) public pure returns (string memory) {
+    error InsufficientBalanceError(uint requiredAmount);
+
+    string public tokenName = "MYTOKEN";
+    uint256 public totalSupply = 100;
+    mapping(address => uint256) public balances;
+   
+    function customRequireCheck(uint _stock) public pure returns (string memory) {
         uint availableStock = 150;
-        require(_stock <= availableStock, "stocks are not available");
+        require(_stock <= availableStock, "Custom error message: Stocks are not available");
         return "Product is in cart now meanwhile you can proceed further";
-
     }
 
-    function assertCheck(uint _number) public pure returns (string memory) {
-        uint currentBid = 450;
-        assert(_number > currentBid);
-        return "Bid is successful and you are at higher rank";
+    function customAssertCondition(uint _value) public pure {
+        require(_value > 0 && _value < 100, "Custom error message: Value must be between 1 and 99");
     }
 
-    function revertCheck(uint _age) public pure {
-        uint minimumAge = 18;
-        if (_age < minimumAge) {
-            revert("You must be 18 years old for this content");
+    function customRevertCondition(uint _balance, uint _amount) public pure {
+        if (_balance < _amount) {
+            revert("Custom error message: Something went wrong");
         }
     }
-}
-    
-
-
 }
